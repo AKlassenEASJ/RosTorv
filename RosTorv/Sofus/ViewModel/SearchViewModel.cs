39f41c714d981cf5e37766614e28a62937ba28c4
sofu0185 <sofu0185@edu.easj.dk>
2018-11-27 15:43:51 +0100

    Added Generic RelayCommand and binding to events on the AutoSuggestBox + Changed ButikKatalogSingleton to contain all stores

--- RosTorv/Sofus/ViewModel/SearchViewModel.cs
+++ RosTorv/Sofus/ViewModel/SearchViewModel.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using RosTorv.Common;
 
 namespace RosTorv.Sofus.ViewModel
 {
-    class SearchViewModel
+    public class SearchViewModel
     {
+        public ICommand TextChangedCommand { get; set; }
+        public ICommand SuggestionChosenCommand { get; set; }
+        public ICommand QuerySubmittedCommand { get; set; }
+
+        public SearchViewModel()
+        {
+            TextChangedCommand = new RelayCommand<AutoSuggestBoxTextChangedEventArgs>(TextChanged);
+            SuggestionChosenCommand = new RelayCommand<AutoSuggestBoxSuggestionChosenEventArgs>(SuggestionChosen);
+            QuerySubmittedCommand = new RelayCommand<AutoSuggestBoxQuerySubmittedEventArgs>(QuerySubmitted);
+        }
+
+        private void TextChanged(AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+
+            }
+        }
+
+        private void SuggestionChosen(AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+
+        }
+
+        private void QuerySubmitted(AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+
+        }
     }
 }
