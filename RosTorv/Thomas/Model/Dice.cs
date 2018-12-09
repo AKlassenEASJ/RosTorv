c960fa8b51e77609b58e47297539b48868352210
Thomas Fogh <thom20m1@edu.easj.dk>
2018-12-09 16:32:18 +0000

    Set up needed base classes for Thomas

---
+++ RosTorv/Thomas/Model/Dice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using RosTorv.Annotations;
+
+namespace RosTorv.Thomas.Model
+{
+    class Dice : INotifyPropertyChanged
+    {
+        private int _faceValue;
+        private Random _random;
+
+        public int FaceValue
+        {
+            get { return _faceValue; }
+            set
+            {
+                _faceValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Dice()
+        {
+            _random = new Random(DateTime.Now.Millisecond);
+
+        }
+
+        public void Roll()
+        {
+
+
+            FaceValue = _random.Next(1, 7);
+
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
